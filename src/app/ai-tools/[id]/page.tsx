9cfa23a03e1f86d57d0c606a4c8c34d1f9863631
hazem gamal <hazemgamal223311@gmail.com>
2025-08-27 21:32:51 +0300

    add title to tool  page

--- src/app/ai-tools/[id]/page.tsx
+++ src/app/ai-tools/[id]/page.tsx
@@ -1,5 +1,8 @@
 import CVToolPage from "@/components/ai-tools/CVToolPage";
 import React from "react";
+export const metadata = {
+  title: "tool",
+};
 const page = () => {
   return <CVToolPage></CVToolPage>;
 };
