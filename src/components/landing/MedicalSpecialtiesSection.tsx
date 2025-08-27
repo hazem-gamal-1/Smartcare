53f687f0d15ab0a28b40c10e5f6c139ff7e9dde5
hazem gamal <hazemgamal223311@gmail.com>
2025-08-27 23:21:53 +0300

    fix import in medical section

--- src/components/landing/MedicalSpecialtiesSection.tsx
+++ src/components/landing/MedicalSpecialtiesSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { Button } from "../ui/Button";
-import SpecialtyCard from "../Specialties/SpecialtyCard";
+import SpecialtyCard from "../specialties/SpecialtyCard";
 import {
   Heart,
   Brain,
@@ -55,7 +55,7 @@ const MedicalSpecialtiesSection = () => {
   if (loading) {
     return <Loader />;
   }
-  
+
   return (
     <section className="py-24 bg-muted/30">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
