99088f2445dd5a2467d8858f38f509fae9924c7f
hazem gamal <hazemgamal223311@gmail.com>
2025-08-27 23:04:03 +0300

    change folder name

--- src/app/Specialties/[specialtyid]/page.tsx
+++ src/app/Specialties/[specialtyid]/page.tsx
@@ -2,7 +2,7 @@ import DoctorsPage from "@/components/doctors/DoctorsPage";
 import React from "react";
 export const metadata = {
   title: "specialty",
-};  
+};
 
 const page = () => {
   return <DoctorsPage></DoctorsPage>;
